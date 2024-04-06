2ac3a0881e768d5582955cc1fb05f023f33471e9
kterhar-edu <81818592+kterhar-edu@users.noreply.github.com>
2024-04-06 17:26:49 +0000

    Added sample data rake and ran it

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -25,8 +25,8 @@ class Photo < ApplicationRecord
   has_many :likes
   has_many :fans, through: :likes
 
-  validates :caption, presense: true
-  validates :image, presense: true
+  validates :caption, presence: true
+  validates :image, presence: true
 
   scope :past_week, -> { where(created_at: 1.week.ago...) }
   scope :by_likes, -> { order(likes_count: :desc) }
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :follow_requests
   resources :comments
   resources :photos
+
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
