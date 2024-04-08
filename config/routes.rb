32e9ba9838228c8194d95df14388319eb63bf4af
kterhar-edu <81818592+kterhar-edu@users.noreply.github.com>
2024-04-08 12:58:11 +0000

    Started user config

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,17 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "photos#index"
 
+  
+
   devise_for :users
   
   resources :comments
   resources :follow_requests
   resources :likes
   resources :photos
-
+  
+  #get "/users/:id" => "users#show", as :user
+  #resources :users, only: :show
+  get "/:username" => "users#show", as: :user
   
 end
